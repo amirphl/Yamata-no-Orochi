348be2e0464ce2805889eb7db8e50cd0403160ab
Amir <amirphl4@gmail.com>
2025-06-18 11:06:17 +0000

    fix: Activate yamata system users so that others can refer to

--- migrations/0024_create_system_company_and_wallet.sql
+++ migrations/0024_create_system_company_and_wallet.sql
@@ -30,7 +30,7 @@ INSERT INTO customers (
     '$2b$12$LQv3c1yqBWVHxkd0LHAkCOYz6TtxMQJqhN8/LewdBPj4/LewdBPj4',
     false,
     false,
-    false,
+    true,
     'jaazebeh.ir',
     NULL,
     CURRENT_TIMESTAMP,
