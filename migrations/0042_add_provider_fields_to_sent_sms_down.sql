6e07b563c3bf000dea949bbd1183f854c3901d59
Amir <amirphl4@gmail.com>
2025-10-29 11:13:49 +0000

    chore(migrations): normalize whitespace in down migration

--- migrations/0042_add_provider_fields_to_sent_sms_down.sql
+++ migrations/0042_add_provider_fields_to_sent_sms_down.sql
@@ -2,7 +2,7 @@
 -- Description: Revert adding optional provider response fields to sent_sms
 
 -- DOWN MIGRATION
-
+ 
 ALTER TABLE sent_sms
 	DROP COLUMN IF EXISTS server_id,
 	DROP COLUMN IF EXISTS error_code,
