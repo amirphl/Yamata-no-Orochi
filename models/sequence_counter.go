3298e17e374f04dad747d6755a7b70be9838b69d
Amir <amirphl4@gmail.com>
2026-03-02 11:06:06 +0000

    limit sequence counter last_value to 16 chars

--- models/sequence_counter.go
+++ models/sequence_counter.go
@@ -5,7 +5,7 @@ import "time"
 // SequenceCounter stores the last value for named monotonic counters.
 type SequenceCounter struct {
 	Name      string    `gorm:"primaryKey;size:64" json:"name"`
-	LastValue string    `gorm:"size:64;not null" json:"last_value"`
+	LastValue string    `gorm:"size:16;not null" json:"last_value"`
 	CreatedAt time.Time `gorm:"default:(CURRENT_TIMESTAMP AT TIME ZONE 'UTC')" json:"created_at"`
 	UpdatedAt time.Time `gorm:"default:(CURRENT_TIMESTAMP AT TIME ZONE 'UTC')" json:"updated_at"`
 }
