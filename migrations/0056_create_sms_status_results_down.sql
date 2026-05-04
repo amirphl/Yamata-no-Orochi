6a00525070b608cb89f5012b92aa63c64f05a389
amirphl <amirphl4@gmail.com>
2026-05-04 14:20:02 +0330

    migrations: fix sms_status_results down migration

--- migrations/0056_create_sms_status_results_down.sql
+++ migrations/0056_create_sms_status_results_down.sql
@@ -2,6 +2,7 @@
 BEGIN;
 
 DROP INDEX IF EXISTS idx_sms_status_results_campaign_customer;
+DROP INDEX IF EXISTS idx_sms_status_results_processed_campaign_customer;
 DROP INDEX IF EXISTS idx_sms_status_results_customer_id;
 DROP INDEX IF EXISTS idx_sms_status_results_processed_campaign_id;
 DROP INDEX IF EXISTS idx_sms_status_results_job_id;
