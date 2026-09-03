86e4edf9b26d7ae4bc7b03a55dc952241a43e96b
amirphl <amirphl4@gmail.com>
2026-09-03 12:46:00 +0330

    Include migration 0133 in the rollback sequence

--- migrations/run_all_down.sql
+++ migrations/run_all_down.sql
@@ -5,6 +5,9 @@
 
 \echo 'Starting database rollback...'
 
+\echo 'Running 0133_decouple_smart_targeting_test_sampling_down.sql...'
+\i migrations/0133_decouple_smart_targeting_test_sampling_down.sql
+
 \echo 'Running 0132_create_tag_test_performance_reports_down.sql...'
 \i migrations/0132_create_tag_test_performance_reports_down.sql
 
